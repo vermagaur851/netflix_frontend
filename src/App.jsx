e6910fb8f87a2b729335edc3fecca7b744642e97
vermagaur851 <138775489+vermagaur851@users.noreply.github.com>
2024-02-22 21:07:25 +0530

    Update App.jsx
    some unwanted files imports are removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,5 @@
 import './app.scss'
-import Register from './pages/register/Register'
 import Home from './pages/home/Home'
-import Watch from './pages/watch/watch'
-import Login from './pages/login/Login'
 
 function App() {
   return <Home/>
